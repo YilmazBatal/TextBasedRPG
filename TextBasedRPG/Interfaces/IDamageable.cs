bd7654a78b763a6cdae4ccf72da16924f6253f19
YilmazBatal <b.ogulbey@gmail.com>
2026-01-27 18:02:56 +0200

    chore: removed unused usings

--- TextBasedRPG/Interfaces/IDamageable.cs
+++ TextBasedRPG/Interfaces/IDamageable.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TextBasedRPG.Interfaces
+﻿namespace TextBasedRPG.Interfaces
 {
     public interface IDamageable
     {
