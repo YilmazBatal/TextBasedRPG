ecb17904ef1b3a89d6a418f0b35d2d2da1cc70a0
YilmazBatal <b.ogulbey@gmail.com>
2026-01-31 18:41:33 +0200

    Refactor : Changed class name to Hero

--- TextBasedRPG/Items/Armor.cs
+++ TextBasedRPG/Items/Armor.cs
@@ -3,6 +3,7 @@
     public class Armor : Item
     {
         public int ArmorDef { get; set; }
+        public int ExtraHP { get; set; } = 0;
         public int RequiredLevel { get; set; }
     }
 }
