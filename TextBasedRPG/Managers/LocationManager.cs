8cf6ced5bc1e582f781c4b2862c450ed6862052e
YilmazBatal <b.ogulbey@gmail.com>
2026-02-26 11:54:55 +0200

    GetLocationIndex method added

--- TextBasedRPG/Managers/LocationManager.cs
+++ TextBasedRPG/Managers/LocationManager.cs
@@ -3,6 +3,10 @@
     internal static class LocationManager
     {
         public static Dictionary<string, string> locations = new ();
+        public static int GetLocationIndex(GameContext context)
+        {
+            return locations.Keys.ToList().IndexOf(context.Player.ActiveLocation);
+        }
         public static void LocationMapping(GameContext context)
         {
             foreach (var item in context.Locations)
